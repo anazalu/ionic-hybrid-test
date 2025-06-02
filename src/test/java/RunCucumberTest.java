a5a157ed27185e6e6f5be558f54ff9bbdb374c76
anazalu <109597957+anazalu@users.noreply.github.com>
2025-06-02 13:26:57 +0300

    update RunCucumberTest

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,29 +1,3 @@
-// src/test/java/RunCucumberTest.java
-// package runners; // Uncomment or set correct package if needed
-
-// REMOVE: import base.BaseTest;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(
-    features = "src/test/java/resources/features",
-    glue = "stepdefinitions", // Make sure this points to where Hooks.java is
-    plugin = {
-        "pretty",
-        "html:target/cucumber-reports.html",
-        "json:target/cucumber.json"
-    },
-    monochrome = true
-)
-// NO LONGER extends BaseTest
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
-    // Body is likely empty now
-}
-
-
-/* 
-
-// package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
@@ -38,5 +12,6 @@ import io.cucumber.testng.CucumberOptions;
     },
     monochrome = true
 )
-public class RunCucumberTest extends AbstractTestNGCucumberTests {}
-*/
+
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+}
